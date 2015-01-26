6ecc4081a167734fe016ff1bfcfb777d7d45d4b5
Jorrit Poelen <jhpoelen@gmail.com>
2015-01-26 12:45:34 -0800

    added first pass at implementing the collection description builder

--- src/test/scala/CommonizerSpec.scala
+++ src/test/scala/CommonizerSpec.scala
@@ -19,6 +19,39 @@ class CommonizerSpec extends FlatSpec with Matchers {
     })
   }
 
+  "create a description" should "include a human readable text" in {
+    val commonName = """true seal"""
+    val scientificName = """Phocidae"""
+    val interactionType = """preysOn"""
+
+    val collectionInfo: Map[String, String] = mkCollectionInfo(commonName, scientificName, interactionType)
+
+    collectionInfo("description") should be("what do true seals eat?\n" +
+      "what do phocids eat?\n" +
+      "what do true seals prey on?\n" +
+      "what do phocids prey on?\n" +
+      "what do true seals hunt?\n" +
+      "what do phocids hunt?\n" +
+      "true seal prey\n" +
+      "phocid prey\n" +
+      "true seal food\n" +
+      "phocid food\n")
+
+    collectionInfo("title") should be( """True Seal Food""")
+  }
+
+  def mkCollectionInfo(commonName: String, scientificName: String, interactionType: String): Map[String, String] = {
+    val interactionTargetTitle = Map("preysOn" -> "Food")
+    val interactionTargetNouns = Map("preysOn" -> List("prey", "food"))
+    val interactionVerbs = Map("preysOn" -> List("eat", "prey on", "hunt"))
+
+    val sentences = interactionVerbs(interactionType).map { verb => "what do " + commonName + "s " + verb + "?\nwhat do " + commonize(scientificName) + "s " + verb + "?\n"}
+    val phrases = interactionTargetNouns(interactionType).map { noun => commonName + " " + noun + "\n" + commonize(scientificName) + " " + noun + "\n"}
+
+    Map("description" -> (sentences.reduce(_ + _) + phrases.reduce(_ + _))
+      , "title" -> (commonName.split(" ").map(_.capitalize).mkString(" ") + " " + interactionTargetTitle(interactionType)))
+  }
+
   def commonize(name: String): String = {
     val idae = """(.*)(idae)$""".r
     val oda = """(.*)(oda)$""".r
